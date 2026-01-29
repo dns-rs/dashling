34639c1493c37de80983ca9e3664b68360bf06c4
dns_rs <denis.glavcic@gmail.com>
2026-01-29 02:53:15 +0100

    weather data logic fixed

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThermometerHalf, faComment, faShield } from '@fortawesome/free-solid-svg-icons'; // Changed to a valid Font Awesome icon
+import { faThermometerHalf, faComment, faShield, faDroplet, faTornado } from '@fortawesome/free-solid-svg-icons';
 import CurrentTime from './CurrentTime'
 import ClothingRecommendation from './ClothingRec';
 import styles from './Weather.module.scss'
-import { faDroplet, faTornado } from '../../node_modules/@fortawesome/free-solid-svg-icons/index';
 
 interface WeatherData {
     temperature_2m: number;
@@ -26,250 +25,92 @@ const { latitudeNum, longitudeNum } = {
     longitudeNum: Number(process.env.NEXT_PUBLIC_LONGITUDE!)
 };
 
+const getWeatherCondition = (code: number, isDay: number): WeatherCondition => {
+    const daySuffix = isDay === 1 ? 'd' : 'n';
+    
+    const descriptions: Record<number, string> = {
+        0: 'Clear sky',
+        1: 'Mainly clear',
+        2: 'Partly cloudy', 
+        3: 'Overcast',
+        45: 'Foggy',
+        48: 'Rime fog',
+        51: 'Light drizzle',
+        53: 'Drizzle',
+        55: 'Heavy drizzle',
+        56: 'Light freezing drizzle',
+        57: 'Freezing drizzle',
+        61: 'Light rain',
+        63: 'Rain',
+        65: 'Heavy rain',
+        66: 'Light freezing rain',
+        67: 'Freezing rain',
+        71: 'Light snow',
+        73: 'Snow',
+        75: 'Heavy snow',
+        77: 'Snow grains',
+        80: 'Light rain showers',
+        81: 'Rain showers',
+        82: 'Heavy rain showers',
+        85: 'Light snow showers',
+        86: 'Snow showers',
+        95: 'Thunderstorm',
+        96: 'Thunderstorm with light hail',
+        99: 'Thunderstorm with heavy hail'
+    };
+
+    const iconMap: Record<number, string> = {
+        0: `01${daySuffix}`,
+        1: `02${daySuffix}`,
+        2: `03${daySuffix}`,
+        3: `04${daySuffix}`,
+        45: `50${daySuffix}`,
+        48: `50${daySuffix}`,
+        51: `09${daySuffix}`,
+        53: `09${daySuffix}`,
+        55: `09${daySuffix}`,
+        56: `09${daySuffix}`,
+        57: `09${daySuffix}`,
+        61: `10${daySuffix}`,
+        63: `10${daySuffix}`,
+        65: `10${daySuffix}`,
+        66: `10${daySuffix}`,
+        67: `13${daySuffix}`,
+        71: `13${daySuffix}`,
+        73: `13${daySuffix}`,
+        75: `13${daySuffix}`,
+        77: `13${daySuffix}`,
+        80: `09${daySuffix}`,
+        81: `09${daySuffix}`,
+        82: `09${daySuffix}`,
+        85: `13${daySuffix}`,
+        86: `13${daySuffix}`,
+        95: `11${daySuffix}`,
+        96: `11${daySuffix}`,
+        99: `11${daySuffix}`
+    };
+
+    const iconCode = iconMap[code] || '01';
+    const description = descriptions[code] || 'Unknown weather';
+    
+    return {
+        description,
+        image: `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+    };
+};
+
 const Weather = () => {
     const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
     const [latitude, setLatitude] = useState<number>(latitudeNum);
     const [longitude, setLongitude] = useState<number>(longitudeNum);
     const [current, setCurrent] = useState<string[]>([
         "temperature_2m",
-        "relative_humidity_2m",
+        "relative_humidity_2m", 
         "apparent_temperature",
         "wind_speed_10m",
         "weather_code",
         "is_day"
     ]);
-
-    const [wcDay, setWcDay] = useState<WeatherCondition[]>([
-        {
-            description: "Sunny",
-            image: "http://openweathermap.org/img/wn/01d@2x.png"
-        },
-        {
-            description: "Mainly Sunny",
-            image: "http://openweathermap.org/img/wn/02d@2x.png"
-        },
-        {
-            description: "Partly Cloudy",
-            image: "http://openweathermap.org/img/wn/03d@2x.png"
-        },
-        {
-            description: "Cloudy",
-            image: "http://openweathermap.org/img/wn/04d@2x.png"
-        },
-        {
-            description: "Foggy",
-            image: "http://openweathermap.org/img/wn/50d@2x.png"
-        },
-        {
-            description: "Rime Fog",
-            image: "http://openweathermap.org/img/wn/50d@2x.png"
-        },
-        {
-            description: "Light Drizzle",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Drizzle",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Heavy Drizzle",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Light Freezing Drizzle",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Freezing Drizzle",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Light Rain",
-            image: "http://openweathermap.org/img/wn/10d@2x.png"
-        },
-        {
-            description: "Rain",
-            image: "http://openweathermap.org/img/wn/10d@2x.png"
-        },
-        {
-            description: "Heavy Rain",
-            image: "http://openweathermap.org/img/wn/10d@2x.png"
-        },
-        {
-            description: "Light Freezing Rain",
-            image: "http://openweathermap.org/img/wn/10d@2x.png"
-        },
-        {
-            description: "Freezing Rain",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Light Snow",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Snow",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Heavy Snow",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Snow Grains",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Light Showers",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Showers",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Heavy Showers",
-            image: "http://openweathermap.org/img/wn/09d@2x.png"
-        },
-        {
-            description: "Light Snow Showers",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Snow Showers",
-            image: "http://openweathermap.org/img/wn/13d@2x.png"
-        },
-        {
-            description: "Thunderstorm",
-            image: "http://openweathermap.org/img/wn/11d@2x.png"
-        },
-        {
-            description: "Light Thunderstorms With Hail",
-            image: "http://openweathermap.org/img/wn/11d@2x.png"
-        },
-        {
-            description: "Thunderstorm With Hail",
-            image: "http://openweathermap.org/img/wn/11d@2x.png"
-        }
-    ]);
-
-    const [wcNight, setWcNight] = useState<WeatherCondition[]>([
-        {
-            description: "Clear",
-            image: "http://openweathermap.org/img/wn/01n@2x.png"
-        },
-        {
-            description: "Mainly Clear",
-            image: "http://openweathermap.org/img/wn/02n@2x.png"
-        },
-        {
-            description: "Partly Cloudy",
-            image: "http://openweathermap.org/img/wn/03n@2x.png"
-        },
-        {
-            description: "Cloudy",
-            image: "http://openweathermap.org/img/wn/04n@2x.png"
-        },
-        {
-            description: "Foggy",
-            image: "http://openweathermap.org/img/wn/50n@2x.png"
-        },
-        {
-            description: "Rime Fog",
-            image: "http://openweathermap.org/img/wn/50n@2x.png"
-        },
-        {
-            description: "Light Drizzle",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Drizzle",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Heavy Drizzle",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Light Freezing Drizzle",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Freezing Drizzle",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Light Rain",
-            image: "http://openweathermap.org/img/wn/10n@2x.png"
-        },
-        {
-            description: "Rain",
-            image: "http://openweathermap.org/img/wn/10n@2x.png"
-        },
-        {
-            description: "Heavy Rain",
-            image: "http://openweathermap.org/img/wn/10n@2x.png"
-        },
-        {
-            description: "Light Freezing Rain",
-            image: "http://openweathermap.org/img/wn/10n@2x.png"
-        },
-        {
-            description: "Freezing Rain",
-            image: "http://openweathermap.org/img/wn/10n@2x.png"
-        },
-        {
-            description: "Light Snow",
-            image: "http://openweathermap.org/img/wn/13n@2x.png"
-        },
-        {
-            description: "Snow",
-            image: "http://openweathermap.org/img/wn/13n@2x.png"
-        },
-        {
-            description: "Heavy Snow",
-            image: "http://openweathermap.org/img/wn/13n@2x.png"
-        },
-        {
-            description: "Snow Grains",
-            image: "http://openweathermap.org/img/wn/13n@2x.png"
-        },
-        {
-            description: "Light Showers",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Showers",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Heavy Showers",
-            image: "http://openweathermap.org/img/wn/09n@2x.png"
-        },
-        {
-            description: "Light Snow Showers",
-            image: "http://openweathermap.org/img/wn/13n@2x.png"
-        },
-        {
-            description: "Snow Showers",
-            image: "http://openweathermap.org/img/wn/13n@2x.png"
-        },
-        {
-            description: "Thunderstorm",
-            image: "http://openweathermap.org/img/wn/11n@2x.png"
-        },
-        {
-            description: "Light Thunderstorms With Hail",
-            image: "http://openweathermap.org/img/wn/11n@2x.png"
-        },
-        {
-            description: "Thunderstorm With Hail",
-            image: "http://openweathermap.org/img/wn/11n@2x.png"
-        }
-    ]);
-
-    const wc = weatherData?.is_day === 1 ? wcDay : wcNight;
     
     const [error, setError] = useState<string | null>(null);
 
@@ -294,10 +135,15 @@ const Weather = () => {
     };
 
     useEffect(() => {
-        fetchWeatherData(); // Fetch data immediately when the component mounts
-        const intervalId = setInterval(fetchWeatherData, 300000); // Fetch data every 5 minutes
-        return () => clearInterval(intervalId); // Cleanup function to clear the interval
+        fetchWeatherData();
+        const intervalId = setInterval(fetchWeatherData, 300000);
+        return () => clearInterval(intervalId);
     }, []);
+
+    const weatherCondition = weatherData ? getWeatherCondition(
+        weatherData.weather_code, 
+        weatherData.is_day
+    ) : null;
 
     return (
         <div className={styles['container']}>
@@ -305,15 +151,15 @@ const Weather = () => {
                 <h1><CurrentTime /></h1>
             </div>
             {error && <div style={{ color: 'red' }}>{error}</div>}
-            {weatherData && (
+            {weatherData && weatherCondition && (
                 <>
                 <div className={styles['weather-container']}>               
-                     <div className={styles['weather-module']}>
+                    <div className={styles['weather-module']}>
                         <div className={styles['icons']}>
-                            <img src={wc[weatherData.weather_code].image} alt={wc[weatherData.weather_code].description} />
+                            <img src={weatherCondition.image} alt={weatherCondition.description} />
                         </div>
                         <div className={styles['text']}>
-                            {wc[weatherData.weather_code].description}
+                            {weatherCondition.description}
                         </div>        
                     </div>   
                     <div className={styles['weather-module']}>
@@ -330,7 +176,7 @@ const Weather = () => {
                             <span className={styles['icon-2']}><FontAwesomeIcon icon={faComment} /> </span>
                         </div>
                         <div className={styles['text']}>
-                            {weatherData.apparent_temperature}°C   
+                            {weatherData.apparent_temperature}°C  
                         </div>                        
                     </div>
                     <div className={styles['weather-module']}>
@@ -340,10 +186,10 @@ const Weather = () => {
                         <div className={styles['text']}>
                             {weatherData.relative_humidity_2m}%
                         </div>                        
-                    </div>            
+                    </div>          
                     <div className={styles['weather-module']}>
                         <div className={styles['icons']}>
-                            <FontAwesomeIcon icon={faTornado} />               
+                            <FontAwesomeIcon icon={faTornado} />            
                         </div>
                         <div className={styles['text']}>
                             {weatherData.wind_speed_10m}km/h
@@ -352,7 +198,8 @@ const Weather = () => {
                     
                 </div>
                 <div className={styles['clothing-container']}>
-                    <h3><span><FontAwesomeIcon icon={faShield}/></span> <span className={styles['clothing-text']}>ARMOR</span> </h3> <ClothingRecommendation weather={weatherData} />
+                    <h3><span><FontAwesomeIcon icon={faShield}/></span> <span className={styles['clothing-text']}>ARMOR</span> </h3> 
+                    <ClothingRecommendation weather={weatherData} />
                 </div>
                 </>
             )}
